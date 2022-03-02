845f898c370724c9edc130d01b7955c33fe53cb8
sthstm <861493868@qq.com>
2022-03-02 22:56:15 +0800

    create game menu

--- game/static/js/dist/game.js
+++ game/static/js/dist/game.js
@@ -1,5 +1,27 @@
+class AcGameMenu {
+	constructor(root) {
+		this.root = root;
+        this.$menu = $(`
+            <div class="ac-game-menu">
+            </div>
+        `);
+	this.root.$ac_game.append(this.$menu);
+
+
+	}
+
+}
 class AcGame {
     constructor(id){
-    
+        this.id = id;
+        this.$ac_game = $('#' + id);
+        // this.AcWingOS = AcWingOS;
+        
+
+        // this.settings = new Settings(this);
+        this.menu = new AcGameMenu(this);
+        // this.playground = new AcGamePlayground(this);
+
+        // this.start();
     }
 }
