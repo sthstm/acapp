ccfb22f635cf3b8113ee6e0fa66f121fd2cc2315
sthstm <861493868@qq.com>
2022-03-04 13:18:16 +0800

    write game

--- game/static/js/src/playground/skill/fireball/zbase.js
+++ game/static/js/src/playground/skill/fireball/zbase.js
@@ -31,14 +31,33 @@ class FireBall extends AcGameObject {
         this.y += this.vy * moved;
         this.move_length -= moved;
 
-        //for (let i = 0; i < this.playground.players.length; i ++ ) {
-            //let player = this.playground.players[i];
-            //if (this.player !== player && this.is_collision(player)) {
-           //     this.attack(player);
-         //   }
-       // }
+        for (let i = 0; i < this.playground.players.length; i ++ ) {
+            let player = this.playground.players[i];
+            if (this.player !== player && this.is_collision(player)) {
+                this.attack(player);
+            }
+        }
 
         this.render();
+    }
+
+    get_dist(x1, y1, x2, y2) {
+        let dx = x1 - x2;
+        let dy = y1 - y2;
+        return Math.sqrt(dx * dx + dy * dy);
+    }
+
+    is_collision(player) {
+        let distance = this.get_dist(this.x, this.y, player.x, player.y);
+        if (distance < this.radius + player.radius)
+            return true;
+        return false;
+    }
+
+    attack(player) {
+        let angle = Math.atan2(player.y - this.y, player.x - this.x);
+        player.is_attacked(angle, this.damage);
+        this.destroy();
     }
 
 
